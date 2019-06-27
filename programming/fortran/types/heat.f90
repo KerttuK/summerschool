c16a33596f623960ba80f17adc8008291b5a724a
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-06-27 13:56:45 +0300

    added subroutine

--- programming/fortran/types/heat.f90
+++ programming/fortran/types/heat.f90
@@ -1,11 +1,39 @@
 module heat
 
-  use iso_fortran_env, only :: real64
+  use iso_fortran_env, only : real64
+  implicit none
 
-  type field
-
-     integer, parameter :: rp = real64
-     integer, allocatable :: nx, ny
-     real(rp), allocatable :: dx, dy
-     real(rp), dimension(:,:), allocatable :: A
+  integer, parameter :: rp = real64
   
+  type field
+    
+     integer, allocatable :: nx, ny
+     real(kind=rp), allocatable :: dx, dy
+     real(kind=rp), dimension(:,:), allocatable :: A
+
+  end type field
+
+contains
+
+  subroutine initialize(nx, ny, field0)
+
+    implicit none
+
+    integer :: nx, ny
+    type(field) :: field0
+
+    field0%dx = 0.01
+    field0%dy = 0.01
+    field0%nx = nx
+    field0%ny = ny
+    
+
+
+  end subroutine initialize
+
+    
+
+
+
+
+end module
