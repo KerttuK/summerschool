75bb25d61acbbf8ed8b83ae0460ddd5edd6025bd
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-06-27 15:21:11 +0300

    modified crossproduct

--- programming/fortran/vecmod/vecmod.F90
+++ programming/fortran/vecmod/vecmod.F90
@@ -40,7 +40,10 @@ contains
   function vector_crossproduct(v1, v2) result(v3)
     type(vector_t), intent(in) :: v1, v2
     type(vector_t) :: v3
-    v3 = v1%x * v2%x + v1%y * v2%y + v1%z * v2%z
+
+    v3%x=v1%y*v2%z-v1%z*v2%y
+    v3%y=v1%z*v2%x-v1%x*v2%z
+    v3%z=v1%x*v2%y-v1%y*v2%x
    
   end function vector_crossproduct
 
