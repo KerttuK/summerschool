2e5d34c870f4f9e7f6ec13874aed60d18093615e
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-06-27 15:18:47 +0300

    added functions

--- programming/fortran/vecmod/vecmod.F90
+++ programming/fortran/vecmod/vecmod.F90
@@ -1,6 +1,7 @@
 module vector_algebra
   use iso_fortran_env, only : REAL64
   implicit none
+  
   type vector_t
      real(REAL64) :: x, y, z
   end type vector_t
@@ -10,11 +11,38 @@ module vector_algebra
   ! ...
 
 contains
+  implicit none
   ! TODO: implement the corresponding functions
 
   function vector_sum(v1, v2) result(v3)
-    !    ...
+    type(vector_t), intent(in) :: v1, v2
+    type(vector_t) :: v3
+    v3%x = v1%x + v2%x
+    v3%y = v1%y + v2%y
+    v3%z = v1%z + v2%z
   end function vector_sum
+
+  function vector_sub(v1, v2) result(v3)
+    type(vector_t), intent(in) :: v1, v2
+    type(vector_t) :: v3
+    v3%x = v1%x - v2%x
+    v3%y = v1%y - v2%y
+    v3%z = v1%z - v2%z
+  end function vector_sub
+
+  function vector_dotproduct(v1, v2) result(v3)
+    type(vector_t), intent(in) :: v1, v2
+    type(vector_t) :: v3
+    v3 = v1%x * v2%x + v1%y * v2%y + v1%z * v2%z
+   
+  end function vector_dotproduct
+
+  function vector_crossproduct(v1, v2) result(v3)
+    type(vector_t), intent(in) :: v1, v2
+    type(vector_t) :: v3
+    v3 = v1%x * v2%x + v1%y * v2%y + v1%z * v2%z
+   
+  end function vector_crossproduct
 
   ! ...
 
