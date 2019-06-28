dca715cde9c28aab6e3028395258b1a76fbbaca7
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-06-28 10:27:25 +0300

    added info about ranks

--- mpi/hello-world/hello.f90
+++ mpi/hello-world/hello.f90
@@ -3,12 +3,22 @@ program hello
   implicit none
 
 
-  integer :: rc
+  integer :: rc, rank, size
   
   call mpi_init(rc)
 
-  write(*,*) 'Hello world!'
+  call mpi_comm_rank(mpi_comm_world, rank, rc)
+  call mpi_comm_size(mpi_comm_world, size, rc)
 
+  if (rank == 0) then
+     
+     write(*,*) 'Hello world!', rank, size
+
+  else
+     write(*,*) 'Hello world!', rank
+  end if
+  
+     
 
   call mpi_finalize(rc)
 
