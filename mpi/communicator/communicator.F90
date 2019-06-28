7115ccd6b1fcbbbd2814618cac392ed94443e56f
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-06-28 18:30:46 +0300

    initialized buffers

--- mpi/communicator/communicator.F90
+++ mpi/communicator/communicator.F90
@@ -2,14 +2,20 @@ program communicator
   use mpi
   implicit none
 
-  integer, parameter :: n_mpi_tasks
-  integer :: ierr, size, rank
+  integer, parameter :: n_mpi_tasks = 4
+  integer :: ierr, ntasks, rank
   integer, dimension(2*n_mpi_tasks) :: sendbuf, recvbuf
-
-  call mpi_comm_size(mpi_comm_world, size, ierr)
-  call mpi_comm_rank(mpi_comm_world, rank, ierr)
+  integer, dimension(2*n_mpi_tasks**2) :: printbuf
 
   call mpi_init(ierr)
+  call mpi_comm_size(mpi_comm_world, ntasks, ierr)
+  call mpi_comm_rank(mpi_comm_world, rank, ierr)
+
+  
+
+  call init_buffers
+
+  call print_buffers(sendbuf)
 
   call mpi_finalize(ierr)
 
@@ -17,14 +23,40 @@ program communicator
 
 contains
 
-  subroutine initialize
+  subroutine init_buffers
     implicit none
+
+    integer :: i
+    !integer, parameter :: n_mpi_tasks
+    !integer, dimension(2*n_mpi_tasks) :: sendbuf, recvbuf
 
     do i=1, 2*n_mpi_tasks
        recvbuf(i) = -1
        sendbuf(i) = i + 2*n_mpi_tasks * rank - 1
     end do
-  end subroutine initialize
+  end subroutine init_buffers
+
+   subroutine print_buffers(buffer)
+    implicit none
+    integer, dimension(:), intent(in) :: buffer
+    integer, parameter :: bufsize = 2*n_mpi_tasks
+    integer :: i
+    character(len=40) :: pformat
+
+    write(pformat,'(A,I3,A)') '(A4,I2,":",', bufsize, 'I3)'
+
+    call mpi_gather(buffer, bufsize, MPI_INTEGER, &
+         & printbuf, bufsize, MPI_INTEGER, &
+         & 0, MPI_COMM_WORLD, ierr)
+
+    if (rank == 0) then
+       do i = 1, ntasks
+          write(*,pformat) 'Task', i - 1, printbuf((i-1)*bufsize+1:i*bufsize)
+       end do
+       print *
+    end if
+  end subroutine print_buffers
+
   
 
 end program communicator
