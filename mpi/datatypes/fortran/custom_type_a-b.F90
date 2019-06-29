9180224a39ff6c29e0f1806eb71728cd97c1b25b
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-06-29 10:11:42 +0300

    added 1a

--- mpi/datatypes/fortran/custom_type_a-b.F90
+++ mpi/datatypes/fortran/custom_type_a-b.F90
@@ -1,11 +1,14 @@
 program datatype1
-  use mpi_f08
+  use mpi
   implicit none
 
   integer, dimension(8,8) :: array
+  integer, dimension(8) :: vector
   integer :: rank, ierr
   !TODO: declare variable for datatype
   integer :: i, j
+  integer :: rowtype
+  type(mpi_status) :: status
 
   call mpi_init(ierr)
   call mpi_comm_rank(MPI_COMM_WORLD, rank ,ierr)
@@ -31,7 +34,18 @@ program datatype1
 
   !TODO: create datatype describing one row, use mpi_type_vector
 
+  call mpi_type_vector(8, 1, 8, mpi_integer, rowtype, ierr)
+  call mpi_type_commit(rowtype, ierr)
+
   !TODO: send first row of matrix from rank 0 to 1
+
+  if (rank ==0) then
+     call mpi_send(array(2,1), 1, rowtype, 1, 1, mpi_comm_world, ierr)
+
+  else if (rank ==1) then
+     call mpi_recv(array(2,1), 1, rowtype, 0, 1, mpi_comm_world, status, ierr)
+  end if
+  
 
   ! Print out the result
   if (rank == 1) then
