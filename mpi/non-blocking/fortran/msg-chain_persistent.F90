4128e1716ff9348168049f52e6454275abe25900
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-06-29 16:19:51 +0300

    added persistent communicators

--- mpi/non-blocking/fortran/msg-chain_persistent.F90
+++ mpi/non-blocking/fortran/msg-chain_persistent.F90
@@ -8,6 +8,8 @@ program basic
   integer :: message(msgsize)
   integer :: receiveBuffer(msgsize)
   integer :: status(mpi_status_size)
+  integer :: recv_obj
+  integer :: send_obj 
   
   real(REAL64) :: t0, t1
 
@@ -41,11 +43,22 @@ program basic
   !       sends and receives
 
  
-  call mpi_Isend(message, msgsize, mpi_integer, destination,&
-       myid+1, mpi_comm_world, requests(2), rc)
+  call mpi_send_init(message, msgsize, mpi_integer, destination,&
+       myid+1, mpi_comm_world, send_obj, rc)
      
-  call mpi_Irecv(receiveBuffer, msgsize, mpi_integer, source, &
-       mpi_any_tag, mpi_comm_world, requests(1), rc)
+  call mpi_recv_init(receiveBuffer, msgsize, mpi_integer, source, &
+       mpi_any_tag, mpi_comm_world, recv_obj, rc)
+
+  call mpi_start(recv_obj, rc)
+  call mpi_start(send_obj, rc)
+
+  call mpi_wait(send_obj, mpi_status_ignore, rc)
+  call mpi_wait(recv_obj, mpi_status_ignore, rc)
+
+  call mpi_request_free(recv_obj, rc)
+  call mpi_request_free(send_obj, rc)
+
+  
 
   write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
        ' Sent elements: ', msgsize, &
@@ -54,7 +67,7 @@ program basic
   ! TODO: Add here a synchronization call so that you can be sure
   !       that the message has been received
 
-  call mpi_waitall(2, requests, status, rc)
+  !call mpi_waitall(2, requests, status, rc)
 
   call mpi_get_count(status, MPI_INTEGER, count, rc)
   write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Receiver: ', myid, &
