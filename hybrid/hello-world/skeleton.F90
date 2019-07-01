1552972dba110e56041c810a20b00a50bb4054ac
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-07-01 15:19:58 +0300

    Hello world with Hybrid programming

--- hybrid/hello-world/skeleton.F90
+++ hybrid/hello-world/skeleton.F90
@@ -7,19 +7,20 @@ program hello
 
   ! TODO: Initialize MPI with thread support.
 
-  call mpi_init(rc)
+  call mpi_init_thread(required, provided, rc)
+  !$omp parallel private(omp_rank)
 
   ! TODO: Find out the MPI rank and thread ID of each thread and print
   !       out the results.
 
+  call mpi_comm_rank(mpi_comm_world, my_id, rc)
+
   ! TODO: Investigate the provided thread support level.
 
-
   
-  !$omp parallel private(omp_rank)
   omp_rank = omp_get_thread_num()
 
-  print *, 'Hello world! by thread ', omp_rank
+  print *, 'Hello world! by thread ', omp_rank, ' in process ', my_id
   !$omp end parallel
   
 
