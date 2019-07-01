32f5396e52febb7731f416bc1752abaee4339af4
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-07-01 19:13:06 +0300

    finished the exercise

--- hybrid/data-sharing/variables.F90
+++ hybrid/data-sharing/variables.F90
@@ -1,4 +1,5 @@
 program exer1
+  use omp_lib
   implicit none
   integer :: var1, var2
   var1 = 1
@@ -6,11 +7,19 @@ program exer1
 
   ! TODO:
   !   Test different data sharing clauses here
+
+  !$omp parallel shared(var1, var2)
+  
   print *, 'Region 1:       var1=', var1, 'var2=', var2
   var1 = var1 + 1
   var2 = var2 + 1
+  
+  !$omp end parallel
+
   !end here :)
   print *, 'After region 1: var1=', var1, 'var2=', var2
   print *
+
+  
 
 end program exer1
