fbf4339258a56dcd0b9973ecfb55119ca6c9cf5a
Kerttu Kouki <kerttu.kouki@fmi.fi>
2019-07-01 14:30:51 +0300

    Hello world with OpenMP

--- hybrid/hello-world/skeleton.F90
+++ hybrid/hello-world/skeleton.F90
@@ -1,16 +1,28 @@
 program hello
   use omp_lib
-  use mpi_f08
+  use mpi
   implicit none
-  integer :: my_id, tid, rc
+  integer :: my_id, tid, rc, omp_rank
   integer :: provided, required=MPI_THREAD_FUNNELED
 
   ! TODO: Initialize MPI with thread support.
+
+  call mpi_init(rc)
 
   ! TODO: Find out the MPI rank and thread ID of each thread and print
   !       out the results.
 
   ! TODO: Investigate the provided thread support level.
 
+
+  
+  !$omp parallel private(omp_rank)
+  omp_rank = omp_get_thread_num()
+
+  print *, 'Hello world! by thread ', omp_rank
+  !$omp end parallel
+  
+
+  
   call MPI_Finalize(rc)
 end program hello
